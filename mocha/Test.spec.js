66e73f2282c57f375471075fd9b6fc963cc5a02d
Davidson1004 <veghdavidpatrik@gmail.com>
2024-04-18 10:47:59 +0200

    Tesz

---
+++ mocha/Test.spec.js
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+import app from "../js/index.js";
+
+describe("Test", function(){
+    describe("Ha nincs megadva semmi", function(){
+        assert.equal(app.Search(), )
+    })
+})
